1d66bbde1494e9de825880f1be2ca41847593dd7
Rune Madsen <rune@runemadsen.com>
2012-03-14 12:08:19 -0400

    added quote

--- lib/asciidoc/plugins/lines/quote.rb
+++ lib/asciidoc/plugins/lines/quote.rb
@@ -1,16 +1,24 @@
 plugin = {
   :name => :quote,
-	:regexp  => /^\[quote\]$/,
+	:regexp  => /^\[quote(,\s?)(?<author>.+)\]$/,
 	:handler => lambda { |lines, element, counter|
+    
+    # right now quotes only work if you specify an author
+    author = lines.current_line.gsub(plugin[:regexp], '\k<author>')
+    lines.shift_line # get rid of [quote]
     
     body = ""
     
     while(lines.shift_line) do
-      break if lines.current_line =~ /^\s*$/
+      break if lines.current_line =~ /^_+$/
       body += lines.current_line
     end
     
+    puts "Quote body:"
+    puts body
+    
     quote = AsciiDoc::AsciiElement.new(plugin[:name])
+    quote.attributes[:author] = author
     quote.children << body
     element.children << quote
     
