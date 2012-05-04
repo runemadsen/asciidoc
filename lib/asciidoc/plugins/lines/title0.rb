07f5538ea4c6a8887c91c8ea53dec63934f547c1
Rune Madsen <rune@runemadsen.com>
2012-05-04 16:52:06 -0400

    before moving to docbook branch

--- lib/asciidoc/plugins/lines/title0.rb
+++ lib/asciidoc/plugins/lines/title0.rb
@@ -2,8 +2,8 @@ plugin = {
   :name => :title1,
 	:regexp  => /^===== +(?<title0>[\S].*?)( +=====)?$/,
 	:handler => lambda { |lines, element, counter|
-    title = AsciiDoc::AsciiElement.new(plugin[:name])
-    title.attributes[:count] = counter.increment(p[:name])
+    title = AsciiDoc::AsciiElement.new(plugin[:name])  
+    title.attributes[:count] = counter.increment(plugin[:name])
     name = lines.current_line.gsub!(plugin[:regexp], '\k<title0>')
     title.attributes[:href] = name.gsub(" ", "-").downcase
     title.children << name
