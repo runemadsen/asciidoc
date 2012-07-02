2457ed32f39fe2c3bc4f0723d6446f25911795f9
Rune Madsen <rune@runemadsen.com>
2012-07-02 12:15:18 -0400

    implemented titles for uls and ols

--- lib/asciidoc/elements/list_element.rb
+++ lib/asciidoc/elements/list_element.rb
@@ -3,6 +3,14 @@ module AsciiDoc
   class ListElement < AsciiElement
     
     def parse_metadata(xml)
+      
+      # get title and delete it so it's not a part of list
+      title = xml.css("title").first
+      if title
+        @title = title.children.first.to_s
+        xml.css("title").first.remove if @title
+      end
+      
       @type = xml_to_view_name(xml.name.to_sym)
       @style = xml.attribute("numeration")
       @attributes = xml.attributes
