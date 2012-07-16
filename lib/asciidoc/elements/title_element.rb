03d0281ad339b70dce7a5999f3d126cccf001649
Rune Madsen <rune@runemadsen.com>
2012-07-16 12:25:35 -0400

    tic fixes

--- lib/asciidoc/elements/title_element.rb
+++ lib/asciidoc/elements/title_element.rb
@@ -2,6 +2,8 @@ module AsciiDoc
   
   class TitleElement < AsciiElement
     
+    attr_reader :content, :attributes # for TOC
+    
     def parse(xml)
       @attributes["id"] = slug(xml.content)
       @content = xml.content
--- lib/asciidoc/filters/toc_filter.rb
+++ lib/asciidoc/filters/toc_filter.rb
@@ -5,20 +5,25 @@ module AsciiDoc
       def self.filter(element)
         hie = []
         find_titles_in_children(element, hie)
+        
+        # stupid hack because docbook creates this "preface" element with an empty title
+        # for everythin that comes before the first chapter title
+        hie.shift
+        
         hie
       end
       
       def self.find_titles_in_children(element, hie)
         
-        levels = [:h1, :h2]
+        levels = [:h2, :h3]
         
         element.children.each do |child|
             
-            # if child.type == levels[0]
-            #               hie << { :el => child, :children => [] }
-            #             elsif child.type == levels[1]
-            #               hie.last[:children] << { :el => child, :children => [] }
-            #             end
+            if child.type == levels[0]
+              hie << { :el => child, :children => [] }
+            elsif child.type == levels[1]
+              hie.last[:children] << { :el => child, :children => [] }
+            end
             
             if child.children
               find_titles_in_children(child, hie)
